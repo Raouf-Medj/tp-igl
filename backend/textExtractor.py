ea94f5799a0344b0dc555d1ab6e4e060dcf4ad94
ABOUD Ibrahim <li_aboud@esi.dz>
2023-12-28 16:34:00 +0100

    added text extracting feature

--- backend/textExtractor.py
+++ backend/textExtractor.py
@@ -35,12 +35,12 @@ def writeStringToFile(text):
 
 
 def gptTextAnalyser(text):
-    prompt = f"Extract the exact following sections from the provided text :\n\n{text}\nthe json file contains only: \nAuthors:'a list of strings'\nTitle:\nAbstract:\nInstitutions:'a list of strings'\nKeyWords:'a list of strings'\nReferences:'a list of strings, mention everything'\nPublication_date:'YYYY-MM-DD'"
+    prompt = f"Extract the exact following sections from the provided text :\n\n{text}\nthe json file contains only: \nAuthors:'a list of strings'\nTitle:\nAbstract:-the paragraph written in the section: abstract-\nInstitutions:'a list of strings'\nKeyWords:'a list of strings, all the keywords in the -keywords section-'\nReferences:'a list of strings, all references in the section : references'\nPublication_date:'YYYY-MM-DD'"
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo-1106",
     response_format={ "type": "json_object" },
     messages=[
-        {"role": "system", "content": "You extract data acurately as it is from the string, you DO NOT REFORMULATE EXTRACTED INFORMATION, you mention every single detail, and don't stop because of length, designed to output JSON."},
+        {"role": "system", "content": "You extract data acurately as it is from the string, you DO NOT REFORMULATE EXTRACTED INFORMATION, you extract all the paragrph, no matter how long, and don't stop writing because it was too long, designed to output JSON."},
         {"role": "user", "content": prompt}
     ]
     )
@@ -48,37 +48,42 @@ def gptTextAnalyser(text):
     return generated_text
 
 
-def textToJson(text):
-    json_object = json.loads(text)
-    json_data = json.dumps(json_object, indent=2)
-    # Specify the file path where you want to save the JSON data
-    return json_data
 
-def fixJsonObject(json_object,text):
+
+def fixJsonObject(json_object,text,url):
     intermediateObject = {}
     #use the intermediateObject to access data from "json_object" without facing problems of case sensitivity
     #the only thing left to do is to guarantee that all fields are here, if not you fill them with blank
     for key in json_object:
         intermediateObject[key.lower()]=key
     finalObject = {}
-    finalObject["id"]="something"
-    finalObject["title"]="something"
-    finalObject["abstract"]="something"
-    finalObject["authors"]="something"
-    finalObject["institutions"]="something"
-    finalObject["keywords"]="something"
-    finalObject["references"]="something"
-    finalObject["url"]="something"
-    finalObject["publication_date"]="something"
-    finalObject["text"]="something"
-    finalObject["validated"]="something"
+    finalObject["title"]=json_object[intermediateObject["title"]] if "title" in intermediateObject else "non trouvé"
+    finalObject["abstract"]=json_object[intermediateObject["abstract"]] if "abstract" in intermediateObject else "non trouvé"
+    finalObject["authors"]=json_object[intermediateObject["authors"]] if "authors" in intermediateObject else []
+    finalObject["institutions"]=json_object[intermediateObject["institutions"]] if "institutions" in intermediateObject else []
+    finalObject["keywords"]=json_object[intermediateObject["keywords"]] if "title" in intermediateObject else []
+    finalObject["references"]=json_object[intermediateObject["references"]] if "references" in intermediateObject else []
+    finalObject["url"]=url
+    finalObject["publication_date"]=json_object[intermediateObject["publication_date"]] if "publication_date" in intermediateObject else "non trouvé"
+    finalObject["text"]=text
+    finalObject["validated"]=False
+    return finalObject
 
+fileName="Article_14"
+urlToPdf = "./static/"+fileName+".pdf"
+urlToJson = "./json_results/"+fileName+".json"
 
-gptStuff = gptTextAnalyser(extractTextFromPDF("./static/dummy.pdf"))
-writeStringToFile(gptStuff)
-file_path = "article.json"
+pureText = extractTextFromPDF(urlToPdf)
+gptText = gptTextAnalyser(pureText)
+gptJson = json.loads(gptText)
+
 # Write the JSON data to the file
-with open(file_path, "w") as json_file:
-    json_file.write(textToJson(gptStuff))
+with open("gpt.json", "w") as json_file:
+    json_file.write(json.dumps(gptJson,indent=2))
+
+verified_dict = fixJsonObject(gptJson,pureText,urlToPdf)
+
+with open(urlToJson,"w") as json_file:
+    json_file.write(json.dumps(verified_dict,indent=2))
 
 
